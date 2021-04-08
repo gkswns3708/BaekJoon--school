15ba24c7458fbde91b60fdf25cb33a1678c603b2
gkswns3708 <gkswns3708@naver.com>
2021-04-08 23:11:31 +0900

    BaekJoon 12852번 1로 만들기 2

---
+++ BaekJoon/BaekJoon/BaekJoon 12852번 1로 만들기 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <algorithm>
+#define int long long
+#define pii pair<int,vector<int>>
+using namespace std;
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N; cin >> N;
+
+	function<int(int)> make_1 = [&](int st) ->int {
+		queue<pii> que;
+		que.push({ st, });
+		
+	};
+
+	return 0;
+}
