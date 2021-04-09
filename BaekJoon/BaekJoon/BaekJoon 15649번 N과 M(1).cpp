9ec46f2237aaeb7311b201c6b42b87fa0f08fb6f
gkswns3708 <gkswns3708@naver.com>
2021-04-09 11:30:59 +0900

    BaekJoon 15649번 N과 M(1)

--- BaekJoon/BaekJoon/BaekJoon 15649번 N과 M(1).cpp
+++ BaekJoon/BaekJoon/BaekJoon 15649번 N과 M(1).cpp
@@ -1,36 +1,36 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <functional>
 #define int long long
 using namespace std;
+int k;
+vector<int> vec(8+1);
+bool visited[8 + 1] = { 0, };
+int N, M;
+void BT(int cnt, int pos) {
+	if (cnt == M) {
+		for (int i = 0; i < M;i++) cout << vec[i] << " ";
+		cout << "\n";
+		return;
+	}
+	else {
+		for (int i = 1; i <= N;i++) {
+			if (visited[i]) continue;
+			vec[k++]= i;
+			visited[i] = 1;
+			BT(cnt + 1, i);
+			k--;
+			visited[i] = 0;
+		}
+	}
+};
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
-	int N, M; cin >> N >> M;
-	vector<int> vec;
-	bool visited[8 + 1] = { 0, };
-	function <void(int, int)> BT = [&](int cnt, int pos) ->void {
-		if (cnt == M) {
-			for (auto now : vec) cout << now << " ";
-			cout << "\n";
-			return;
-		}
-		else {
-			for (int i = 1; i <= N;i++) {
-				if (visited[i]) continue;
-				vec.push_back(i);
-				visited[i] = 1;
-				BT(cnt + 1, i);
-				vec.pop_back();
-				visited[i] = 0;
-			}
-		}
-	};
+	cin >> N >> M;
 	BT(0, 0);
 	return 0;
 }
