effef01cdf64b62932749c3d985a6ba10f97fa72
gkswns3708 <gkswns3708@naver.com>
2021-04-11 23:30:11 +0900

    BaekJoon 16926번 배열 돌리기 1

---
+++ BaekJoon/BaekJoon/BaekJoon 16926번 배열 돌리기 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define int long long
+using namespace std;
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+
+
+	return 0;
+}
+}
