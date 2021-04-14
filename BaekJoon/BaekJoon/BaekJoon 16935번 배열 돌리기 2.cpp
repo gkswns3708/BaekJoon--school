97be5ce48b7267108ec0dc9207e6f4dacbd68e49
gkswns3708 <gkswns3708@naver.com>
2021-04-14 15:15:53 +0900

    BaekJoon 16935번 배열 돌리기 2

--- BaekJoon/BaekJoon/BaekJoon 16935번 배열 돌리기 2.cpp
+++ BaekJoon/BaekJoon/BaekJoon 16935번 배열 돌리기 2.cpp
@@ -3,35 +3,40 @@
 #include <algorithm>
 #include <functional>
 #define int long long
+#define pii pair<int , int>
 using namespace std;
+
+int arr[1000 + 2][1000 + 2];
+pii check[1000 + 1][1000 + 1];
+int ans[1000 + 2][1000 + 2];
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
+
 	int N, M, R; cin >> N >> M >> R;
+	vector<vector<int>> vec(min(N, M) / 2 + 1);
+	for (int i = 1; i <= N;i++) for (int j = 1; j <= M;j++) cin >> arr[i][j];
 
-	int arr[100 + 1][100 + 1];
-	for (int i = 0; i < N;i++) for (int j = 0; j < M;j++) cin >> arr[i][j];
-
-	function <void(int)> query = [&](int order)->void {
-		switch (order) {
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-
-		}
+	function <void(int)> search = [&](int x) -> void {
+		pii st = { x,x };
+		int cnt = 0;
+		while (st.second <= M + 1 - x) check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first][st.second++]);
+		st.second--, st.first++;
+		while (st.first <= N + 1 - x) check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first++][st.second]);
+		st.first--, st.second--;
+		while (st.second >= x) check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first][st.second--]);
+		st.second++, st.first--;
+		while (st.first >= x + 1) check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first--][st.second]);
 	};
+	for (int i = 1; i <= min(N, M) / 2;i++) search(i);
+	for (int i = 1; i <= N;i++) for (int j = 1; j <= M;j++) ans[i][j] = vec[check[i][j].first][(check[i][j].second + R) % vec[check[i][j].first].size()];;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M;j++) cout << ans[i][j] << " ";
+		cout << '\n';
+	}
 
 	return 0;
 }
