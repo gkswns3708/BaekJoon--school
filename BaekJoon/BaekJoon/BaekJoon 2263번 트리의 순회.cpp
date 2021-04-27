0e43769b06bf462715329d3d785eb70d67b0e7c6
gkswns3708 <gkswns3708@naver.com>
2021-04-27 16:07:11 +0900

    BaekJoon 2263번 트리의 순회

--- BaekJoon/BaekJoon/BaekJoon 2263번 트리의 순회.cpp
+++ BaekJoon/BaekJoon/BaekJoon 2263번 트리의 순회.cpp
@@ -4,19 +4,16 @@
 #define tiii tuple<int,int,int>
 using namespace std;
 
-typedef struct Node {
-	Node* left;
-	Node* right;
-	int num;
-}Node;
-
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	int N; cin >> N;
-	vector<int> postorder(N + 1), inorder(N + 1), preorder(N + 1);
-	for (int i = 0; i < N;i++) cin >> inorder[i];
+	vector<int> postorder(N + 1), inorder(N + 1), index(N + 1);
+	for (int i = 0; i < N;i++) {
+		cin >> inorder[i];
+		index[inorder[i]] = i;
+	}
 	for (int i = 0; i < N;i++) cin >> postorder[i];
 
 	function <void (int, int,int ,int)> maketree = [&](int pst, int ped, int ist, int ied)->void {
@@ -24,20 +21,11 @@ int32_t main() {
 		if (pst > ped || ist > ied) return;
 		int root = postorder[ped];
 		cout << root << " ";
-		int idx = -1;
-		for (int i = ist; i <= ied;i++) {
-			if (inorder[i] == root) {
-				idx = i;
-				break;
-			}
-		}
 
-		maketree(pst, pst + (idx - ist) - 1, ist, idx - 1);
-		maketree(pst + (idx - ist), ped - 1, idx + 1, ied);
+		maketree(pst, pst + (index[root] - ist )-1, ist, index[root] - 1);
+		maketree(pst + (index[root] - ist), ped - 1, index[root] + 1, ied);
 
 	};
-
 	maketree(0, N - 1, 0, N - 1);
-
 	return 0;
 }
