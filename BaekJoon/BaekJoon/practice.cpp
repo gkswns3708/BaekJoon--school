bd6a253bae79e8ccb3f494126d1afac854600675
gkswns3708 <gkswns3708@naver.com>
2021-04-27 15:09:43 +0900

    BaekJoon 2263번 트리의 순회

--- BaekJoon/BaekJoon/practice.cpp
+++ BaekJoon/BaekJoon/practice.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <sstream>
+#include <bits/stdc++.h>
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -9,27 +6,8 @@ using namespace std;
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-    string s = "2016-09-15 01:00:04.002 2.0s";
-
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == ':' || s[i] == '-')
-        {
-            s[i] = ' ';
-        }
-    }
-
-    double num = 0;
-
-    stringstream stream1;
-
-    stream1.str(s);
-
-    while (stream1 >> num)
-    {
-        cout << "num : " << num << endl;
-    }
-
+    char s[10]; cin >> s;
+    cout << strlen(s) << "\n";
+    cout << sizeof(s) << "\n";
     return 0;
 }
